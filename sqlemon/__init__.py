6e45318b0e2f2797721cf32741ea971f48430bf0
Daniel Sank <sank.daniel@gmail.com>
2017-05-15 00:20:48 -0700

    Add function to detect production mnode.

--- sqlemon/__init__.py
+++ sqlemon/__init__.py
@@ -1,3 +1,12 @@
 __version__ = "0.4.1"
 __project_name__ = "sqlemon"
 
+
+def production_mode():
+    """Return True if running in production mode."""
+    if (os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
+        mode=True
+    else:
+        mode=False
+    return mode
+
