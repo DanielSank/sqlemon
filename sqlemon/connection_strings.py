ca7df2f17422f7444a0027197e31d8887b493888
Daniel Sank <sank.daniel@gmail.com>
2017-06-03 13:56:15 -0700

    Improve comments.

--- sqlemon/connection_strings.py
+++ sqlemon/connection_strings.py
@@ -1,19 +1,19 @@
 MYSQL_CLIENT_CLOUD = "mysql --user={} --password={} -S /tmp/cloudsql/{}"
 # MySQL client through proxy to cloud
-# user, password, instance connection name
+# Args: user, password, instance connection name
 
 MYSQL_CLIENT_LOCAL = "mysql --user={} --password={} --host={} --port={}"
 # MySQL client local
-# user, password, host, port
+# Args: user, password, host, port
 
 APP_ENGINE_CLOUD = "mysql+mysqldb://{}:{}@/{}?unix_socket=/cloudsql/{}"
 # App Engine to Cloud
-# user, password, database, instance connection name
+# Args: user, password, database, instance connection name
 
 LOCAL_TOOLS_CLOUD = "mysql+pymysql://{}:{}@/{}?unix_socket=/tmp/cloudsql/{}"
 # e.g. Alembic to Cloud
-# user, password, project, instance connection name
+# Args: user, password, project, instance connection name
 
 LOCAL_TOOLS_LOCAL = "mysql+pymysql://{}:{}@{}:{}/{}"
 # e.g. Alembic to Cloud
-# user, password, host, port, database
+# Args: user, password, host, port, database
