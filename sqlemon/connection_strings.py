cb4d5e354e4173cde8b6a5b551d7af8215246ef4
Daniel Sank <sank.daniel@gmail.com>
2017-06-04 02:01:50 -0700

    Clean up proxy and client scripts.

--- sqlemon/connection_strings.py
+++ sqlemon/connection_strings.py
@@ -1,3 +1,7 @@
+"""
+Invoke MySQL client, connecting either to local or cloud databases. These
+commands connect to the server, but not a specific database.
+"""
 MYSQL_CLIENT_CLOUD = "mysql --user={} --password={} -S /tmp/cloudsql/{}"
 # MySQL client through proxy to cloud
 # Args: user, password, instance connection name
@@ -6,14 +10,19 @@ MYSQL_CLIENT_LOCAL = "mysql --user={} --password={} --host={} --port={}"
 # MySQL client local
 # Args: user, password, host, port
 
+"""
+SQLAlchemy connection URL's for local and cloud databases, connecting either
+from test server or local tools. These all point to the the specific database
+used by your project.
+"""
 APP_ENGINE_CLOUD = "mysql+mysqldb://{}:{}@/{}?unix_socket=/cloudsql/{}"
 # App Engine to Cloud
 # Args: user, password, database, instance connection name
 
 LOCAL_TOOLS_CLOUD = "mysql+pymysql://{}:{}@/{}?unix_socket=/tmp/cloudsql/{}"
 # e.g. Alembic to Cloud
-# Args: user, password, project, instance connection name
+# Args: user, password, database, instance connection name
 
 LOCAL_TOOLS_LOCAL = "mysql+pymysql://{}:{}@{}:{}/{}"
-# e.g. Alembic to Cloud
+# e.g. Alembic to local database
 # Args: user, password, host, port, database
